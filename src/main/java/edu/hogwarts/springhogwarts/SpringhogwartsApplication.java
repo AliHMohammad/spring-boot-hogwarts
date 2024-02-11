459a9afb98fd270fba6bafe78611fc8779482d6d
AliHMohammad <alihmohammad@hotmail.com>
2024-02-11 19:24:19 +0100

    setup

---
+++ src/main/java/edu/hogwarts/springhogwarts/SpringhogwartsApplication.java
@@ -0,0 +1,13 @@
+package edu.hogwarts.springhogwarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhogwartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringhogwartsApplication.class, args);
+    }
+
+}
