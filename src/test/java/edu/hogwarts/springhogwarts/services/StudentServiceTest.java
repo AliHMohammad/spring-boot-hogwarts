ce8a309c4883dd41b547965c399220beced8861e
AliHMohammad <alihmohammad@hotmail.com>
2024-02-17 21:43:03 +0100

    added student repo tests

--- src/test/java/edu/hogwarts/springhogwarts/services/StudentServiceTest.java
+++ src/test/java/edu/hogwarts/springhogwarts/services/StudentServiceTest.java
@@ -2,6 +2,7 @@ package edu.hogwarts.springhogwarts.services;
 
 import edu.hogwarts.springhogwarts.models.Student;
 import edu.hogwarts.springhogwarts.repositories.StudentRepository;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,6 @@ class StudentServiceTest {
     @Autowired
     private StudentRepository studentRepository;
 
-
     @BeforeEach
     void setUp() {
         studentRepository.save(new Student("Ali Haider Mohammad", LocalDate.of(2000, 9, 12), false, 2008, 2016, true));
@@ -26,7 +26,22 @@ class StudentServiceTest {
 
     @Test
     public void getAllStudents() {
-        //assertEquals(2, studentService.getStudents().size());
-        //assertEquals(2, studentRepository.count());
+        assertEquals(2, studentRepository.count());
+    }
+
+    @Test
+    public void createStudent() {
+        Student student = new Student();
+        student.setFullName("Lars Møller");
+        student.setDateOfBirth(LocalDate.of(1990,4,1));
+        studentRepository.save(student);
+        assertEquals(3, studentRepository.count());
+        assertTrue(student.getId() > 0);
+    }
+
+    @Test void deleteStudent() {
+        studentRepository.deleteById(1L);
+        assertEquals(1, studentRepository.count());
     }
 }
+
